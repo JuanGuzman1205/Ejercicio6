27aec8a64705a5c7c2dbc27254f81b8713509415
JuanGuzman1205 <jr20075jr@gmail.com>
2023-09-01 17:45:02 -0500

    Ejercicio finalizado

--- Ejercicio6/Ejercicio6/Program.cs
+++ Ejercicio6/Ejercicio6/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//1.	Escribir un programa que convierta un valor dado en grados Fahrenheit a grados Celsius.//
+
+public class ejrcicio6
+{
+    static void Main(string[] args)
+    {
+        double gradoscelcius, gradosfahrenheit;
+        
+        
+
+
+        Console.Write("Ingrese la temperatura en grados celcius: ");
+        gradoscelcius = Convert.ToDouble(Console.ReadLine());
+
+        gradosfahrenheit = (((9 * gradoscelcius)/5) + 32);
+
+        Console.Write("La temperatura en grados Fahrenheit es: " + gradosfahrenheit);
+    }
+
+
+
+
+
+}
+
